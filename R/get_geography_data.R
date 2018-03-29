3f92b9dac898874f4bc6a4abaa7eef11fef2e27a
Konrad Zdeb <konrad.zdeb@me.com>
2018-03-29 17:37:33 +0100

    Fixed packge startup message mechanism

--- R/get_geography_data.R
+++ R/get_geography_data.R
@@ -1,10 +1,13 @@
 #' @title Get Geography Data
 #'
 #' @description The function fetches statistical data for a selected geography
-#'   \href{http://statistics.gov.scot}{statistics.gov.scot}.
+#'   framework from: \href{http://statistics.gov.scot}{statistics.gov.scot}.
 #'
 #' @param indicator A character vector with the name of indicator to be sourced from the
 #'   \href{http://statistics.gov.scot}{statistics.gov.scot}.
+#'
+#' @param geography A character corresponding to geographies for which to source
+#'   the data.
 #'
 #' @details In effect, the following function works as a wraper for a SPARQL query
 #'   sourcing data across desired geography for selected indicator. The function

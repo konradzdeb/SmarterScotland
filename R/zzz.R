d8f047f8b1fa9d2a10cde0fc169ada18f23164be
Konrad Zdeb <konrad.zdeb@me.com>
2019-01-06 00:31:01 +0000

    Added cleaning of C++ code

--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,8 @@
   packageStartupMessage("Loaded SmarterScotland version:",
                         utils::packageVersion("SmarterScotland"))
 }
+
+# Clean up C++ code
+.onUnload <- function(libpath) {
+  library.dynam.unload("SmarterScotland", libpath)
+}

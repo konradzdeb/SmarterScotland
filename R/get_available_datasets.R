20c59d5c4772d6309699b9dc4d9259632c220fc7
Konrad Zdeb <konrad.zdeb@me.com>
2019-03-22 22:11:31 +0000

    Getting all data sets
    Testing sourcing all data sets and SPARQL query development
    
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

--- R/get_available_datasets.R
+++ R/get_available_datasets.R
@@ -1,3 +1,27 @@
-get_available_datasets <- function(pattern) {
-
+#' @title Get Available Data Sets
+#'
+#' @description Function sources data available at
+#'   \url{https://statistics.gov.scot/}.
+#'
+#' @param pattern A optional pattern to match data set name.
+#'
+#' @return A data frame
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_available_datasets(pattern = "health")
+#' }
+get_available_datasets <- function(pattern,
+                                   endpoint = "http://statistics.gov.scot/sparql.csv") {
+  if (missing(pattern)) {
+    # Source full list of data sets
+    qry <-
+      read_query_file(query_file("get_available_datasets.sparql"))
+    dta_res <- SPARQL(url = endpoint,
+                      query = qry,
+                      format = "csv")
+    return(dta_res)
+  }
 }

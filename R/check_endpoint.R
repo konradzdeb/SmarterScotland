d5cbb646b66b05499da6b0334f2f6c5759e21490
Konrad Zdeb <konrad.zdeb@me.com>
2019-03-24 23:10:50 +0000

    Utility functions
    Endpoint checking function is more rebust some tests are still failing and need fixing
    
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

--- R/check_endpoint.R
+++ R/check_endpoint.R
@@ -3,8 +3,9 @@
 #' @description Function checks whether the provided endpoint is of online.
 #'
 #' @param endpoint A SPARQL enpoint as string.
-#' @param stop_on_bad A logical, if \code{TRUE} function will
-#'   call \code{\link[base]{stop}} if endpoint is not accessible.
+#' @param check_mode For \code{stop, warn} or \code{ignore}, decides whether to
+#'   stop, warn or do nothing in case of broken endpoint url. \code{ignore}
+#'   option ignores checking.
 #' @param timeout Timeout for a ping response,
 #'   as in \code{\link[pingr]{timeout}}
 #'
@@ -19,31 +20,52 @@
 #' \dontrun{
 #'   check_endpoint("http://statistics.gov.scot/sparql")
 #' }
-check_endpoint <- function(endpoint, stop_on_bad = FALSE, timeout = 1) {
+check_endpoint <- function(check_mode = c("warn", "stop", "ignore"),
+                           timeout = 1) {
+  check_mode <- match.arg(check_mode)
+
   # Check if provided object is a string
   assertString(
-    x = endpoint,
+    x = getOption("SmarterScotland.endpoint"),
     na.ok = FALSE,
     min.chars = 3,
     null.ok = FALSE
   )
 
   # Chek if url is accessible
-  ping(
-    destination = endpoint,
-    continuous = FALSE,
-    verbose = FALSE,
-    count = 1,
-    timeout = timeout
-  ) -> ping_res
-
-  if (stop_on_bad) {
-    if (any(is.na(ping_res))) {
-      stop(paste("Endpoint", endpoint, "is not accessible."))
-    }
+  f_ping_fail <- function(timeout) {
+    sink(tempfile())
+    ping(
+      destination = getOption("SmarterScotland.endpoint"),
+      continuous = FALSE,
+      verbose = FALSE,
+      count = 1,
+      timeout = timeout
+    ) -> ping_fail
+    sink()
+    return(is.na(ping_fail))
   }
 
-  # Return T/F value showing if endpoint was accessible
-  return(!any(is.na(ping_res)))
+  switch(
+    check_mode,
+    warn = if (f_ping_fail(timeout)) {
+      warning("Ping to ",
+              getOption("SmarterScotland.endpoint"),
+              " failed.")
+      FALSE
+    } else {
+      TRUE
+    },
+    stop = if (f_ping_fail(timeout)) {
+      stop("Ping to ",
+           getOption("SmarterScotland.endpoint"),
+           " failed.")
+    } else {
+      TRUE
+    },
+    ignore = return(TRUE)
+  ) -> res
+
+  return(invisible(res))
 
 }
--- tests/testthat/test-checking_endpoint_url.R
+++ tests/testthat/test-checking_endpoint_url.R
@@ -1,13 +1,47 @@
-context("Test checking enpoint")
-
-test_that(desc = "Errores on no-scalars", code = {
-  expect_error(object = check_endpoint(
-    endpoint = c("http://www.google.com/", "http://www.google.co.uk/"),
-    stop_on_bad = TRUE
-  ))
-})
+library(SmarterScotland)
+context("Endpoint checking")
 
 test_that(desc = "Returns true for valid URL",
           code = {
-            expect_true(object = check_endpoint(endpoint = "www.google.com"))
+            op_end_old <- getOption("SmarterScotland.endpoint")
+            options(SmarterScotland.endpoint = "http://statistics.gov.scot/sparql.csv")
+            expect_true(object = check_endpoint())
+            options(SmarterScotland.endpoint = op_end_old)
           })
+
+
+test_that(desc = "Returns warning for invalid URL.",
+          code = {
+            op_end_old <- getOption("SmarterScotland.endpoint")
+            options(SmarterScotland.endpoint = "http://statistics.invalid.com/url.to/sparql")
+            expect_warning(object = check_endpoint(check_mode = "warn"),
+                           regexp = "^Ping.*failed\\.$")
+            options(SmarterScotland.endpoint = op_end_old)
+          })
+
+test_that(desc = "Returns false for invalid URLs",
+          code = {
+            op_end_old <- getOption("SmarterScotland.endpoint")
+            options(SmarterScotland.endpoint = "http://statistics.invalid.com/url.to/sparql")
+            expect_false(object =  check_endpoint())
+            options(SmarterScotland.endpoint = op_end_old)
+          })
+
+test_that(desc = "Stops for invalid URLs",
+          code = {
+            op_end_old <- getOption("SmarterScotland.endpoint")
+            options(SmarterScotland.endpoint = "http://statistics.invalid.com/url.to/sparql")
+            expect_error(object = check_endpoint(check_mode = "stop"),
+                         regexp = "^Ping.*failed\\.$")
+            options(SmarterScotland.endpoint = op_end_old)
+          })
+
+test_that(desc = "Returns TRUE for ignore",
+          code = {
+            op_end_old <- getOption("SmarterScotland.endpoint")
+            options(SmarterScotland.endpoint = "http://statistics.invalid.com/url.to/sparql")
+            expect_true(object = check_endpoint(check_mode = "ignore"))
+            options(SmarterScotland.endpoint = op_end_old)
+          })
+
+

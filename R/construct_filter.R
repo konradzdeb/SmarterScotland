0171061d5b1a59bb203bc6fb81fc5d9fbe726f75
Konrad Zdeb <konrad.zdeb@me.com>
2019-08-10 13:31:21 +0100

    Utility functions
    Simple function constructing filter. It's easier to apply it like that instead of faffing around with paste in query development.

---
+++ R/construct_filter.R
@@ -0,0 +1,57 @@
+#' @title Construct SPARQL filter
+#'
+#' @description This query derives \code{SPARQL} filter statement using the
+#'   \code{IN} syntax.
+#'
+#' @details For instace, arguments \code{sparql_variable = "time"} and
+#'   \code{filter_values = c(2011, 2012)} will result in a expression
+#'   \code{FILTER (?time IN ( "2011, 2012" ))}.
+#'
+#' @param sparql_variable A character scalar to be used in \code{IN} clause,
+#'   without ?.
+#' @param filter_values A vector of filter values to be verified against in the
+#'   filter statement.
+#'
+#' @return A character scalar corresponding to SPARQL filter expression.
+#'
+#' @examples
+#' construct_filter(sparql_variable = "time", filter_values = c(2011, 2012))
+construct_filter <- function(sparql_variable, filter_values) {
+  # Check that passed sparql variable is a scalar
+  assertScalar(x = sparql_variable)
+
+  assert_vector(
+    x = filter_values,
+    strict = TRUE,
+    min.len = 1,
+    all.missing = FALSE
+  )
+
+  # Collapse vector elements
+  collapsed_filter_values <- Reduce(
+    f = function(x, y) {
+      paste(x, y,  sep = ", ")
+    },
+    x = sapply(
+      X = filter_values,
+      FUN = function(x) {
+        paste("'", x, "'", sep = "")
+      }
+    ),
+    accumulate = FALSE,
+  )
+
+  # Create proper SPARQL variable
+  sparql_variable_w_q <- paste("?", sparql_variable, sep = "")
+
+  # Create query statement
+  filter_st <- glue(
+    "FILTER ([sparql_variable_w_q] IN ([collapsed_filter_values]))",
+    .open = "[",
+    .close = "]"
+  )
+
+  # Return filter statement
+  return(unclass(filter_st))
+
+}
---
+++ tests/testthat/test_filter_statement.R
@@ -0,0 +1,32 @@
+context("Filter statement development")
+
+test_that(desc = "Errores on non-scalar variable",
+          code = expect_error(object = construct_filter(
+            sparql_variable = c("abc", "def"),
+            filter_values = 1:10
+          )))
+
+test_that(desc = "Errores on NULLS",
+          code = expect_error(object = construct_filter(
+            sparql_variable = NULL,
+            filter_values = NULL
+          )))
+
+test_that(
+  desc = "Produces character vector",
+  code = expect_vector(
+    object = construct_filter(sparql_variable = "abc",
+                              filter_values = 2001:2010),
+    ptype = character(),
+    size = 1
+  )
+)
+
+test_that(
+  desc = "Content is OK",
+  code = expect_equal(
+    object = construct_filter(sparql_variable = "time",
+                              filter_values = 2001:2002),
+    expected = "FILTER (?time IN ('2001', '2002'))"
+  )
+)

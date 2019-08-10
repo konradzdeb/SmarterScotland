fa158d66ada600b7343338396c614960f31b09bc
Konrad Zdeb <konrad.zdeb@me.com>
2019-08-10 12:07:02 +0100

    Testing
    Replace 'indicator' with 'data_set' which is consistent with how the remaining functions are used.

--- tests/testthat/test-geography-data.R
+++ tests/testthat/test-geography-data.R
@@ -1,23 +1,25 @@
 context("Sourcing data per hierarchy")
 
-test_that(desc = "Errors on non-character indicator", {
-  expect_error(object = get_geography_data(indicator = 1),
-               regexp = "Assertion\\son\\s\\'indicator\\'\\sfailed.*")
+test_that(desc = "Errors on non-character data_set", {
+  expect_error(object = get_geography_data(data_set = 1),
+               regexp = "Assertion\\son\\s\\'data_set\\'\\sfailed.*")
 })
 
-test_that(desc = "Errors on non-character indicator", {
-  expect_error(object = get_geography_data(indicator = "JSA", geography = NULL),
+test_that(desc = "Errors on non-character data_set", {
+  expect_error(object = get_geography_data(data_set = "JSA", geography = NULL),
                regexp = "Assertion\\son\\s\\'geography\\'\\sfailed.*")
 })
 
 test_that("Erros on missing argument", {
   expect_error(object = get_geography_data(),
-               regexp = "argument\\s\"indicator\"\\sis\\smissing")
+               regexp = "argument\\s\"data_set\"\\sis\\smissing")
 })
 
 # Write subsequnt tests for with mock
 test_that(desc = "Not trying to run for more than one argument",
           code = {
-            expect_error(get_geography_data(indicator = c("abc", "def")))
-            expect_error(get_geography_data(indicator = "abc", geography = c("a", "b")))
+            expect_error(get_geography_data(data_set = c("abc", "def")))
+            expect_error(get_geography_data(data_set = "abc", geography = c("a", "b")))
           })
+
+test_that

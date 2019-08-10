b3eaa0a36b6113bcf81e4d6833502ee2daf9177b
Konrad Zdeb <konrad.zdeb@me.com>
2019-08-10 20:58:17 +0100

    SPARQL: geographies
    Better function naming for sourcing available geographies.

---
+++ R/get_available_geographies.R
@@ -0,0 +1,19 @@
+#' @title Get Available Geographies
+#'
+#' @description The function sources available goegraphies from
+#'   \emph{Standard Geography Code Register}.
+#'
+#' @details The \emph{Standard Geography Code Register} is available in the
+#'   package and can be loaded using \code
+#'
+#' @return A data frame.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_available_geographies()
+#' }
+get_available_geographies <- function() {
+  return(NULL)
+}

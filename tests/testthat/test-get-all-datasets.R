5c3b6f434666155f2c0887785a961ee5a9b2b203
Konrad Zdeb <konrad.zdeb@me.com>
2019-08-11 12:07:03 +0100

    Tests
    Fixed tests for getting available data sets.

--- tests/testthat/test-get-all-datasets.R
+++ tests/testthat/test-get-all-datasets.R
@@ -5,10 +5,10 @@ with_mock_api({
             code = {
               dta_av_datasets <- get_available_datasets()
               expect_is(object = dta_av_datasets, class = "data.frame")
-              expect_length(object = dta_av_datasets, n = 3)
+              expect_length(object = dta_av_datasets, n = 2)
               expect_named(
                 object = dta_av_datasets,
-                expected = c("dataset.type", "dataset.value", "subject")
+                expected = c("dataset_value", "subject")
               )
               # Expect all subjects (not filtered)
               expect_false(object = all(grepl(
@@ -20,10 +20,10 @@ with_mock_api({
             code = {
               dta_av_datasets_fltrd <- get_available_datasets(pattern = "life")
               expect_is(object = dta_av_datasets_fltrd, class = "data.frame")
-              expect_length(object = dta_av_datasets_fltrd, n = 3)
+              expect_length(object = dta_av_datasets_fltrd, n = 2)
               expect_named(
                 object = dta_av_datasets_fltrd,
-                expected = c("dataset.type", "dataset.value", "subject")
+                expected = c("dataset_value", "subject")
               )
               # Expect subjects with 'life' keyword
               expect_true(object = all(grepl(

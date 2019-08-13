01b7e7c02df8df36b18f67fa286fc67ace355c92
Konrad Zdeb <konrad.zdeb@me.com>
2019-08-13 20:50:05 +0100

    Documentation
    New data set added.

--- R/data.R
+++ R/data.R
@@ -5,8 +5,21 @@
 #'
 #' @format A data frame with 270 rows and 2 variables:
 #' \describe{
-#'   \item{dataset.value"}{URI}
+#'   \item{dataset.value}{URI}
 #'   \item{subject}{Shortened name}
 #' }
 #' @source \url{https://statistics.gov.scot/}
 "available_data_sets"
+
+#' Standard Geography Code Register
+#'
+#' The following data set contains trimmed version of the
+#'   \emph{Standard Geography Code Register}. The full data set is available at
+#'   dedicated URL
+#'   \url{https://statistics.gov.scot/data/standard-geography-code-register}.
+#'
+#' @format A data frame with three columns:
+#' \describe{
+#'   \item{geography}{Geography URI}
+#' }
+#' @source \url{https://statistics.gov.scot/}

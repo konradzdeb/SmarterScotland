aec9786a31ad567f683eef0d9e2f5dccb62389ba
Konrad Zdeb <konrad.zdeb@me.com>
2019-08-18 22:59:16 +0100

    Trying tic-based deployment

---
+++ tic.R
@@ -0,0 +1,7 @@
+do_package_checks()
+
+if (ci_on_travis()) {
+  get_stage("deploy") %>%
+    add_step(step_build_pkgdown()) %>%
+    add_step(step_push_deploy(branch = "gh-pages", path = "docs"))
+}

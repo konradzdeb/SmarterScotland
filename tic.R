5e60e95148919383a7d99b28df84d6f0a8561248
Konrad Zdeb <konrad.zdeb@me.com>
2019-08-19 13:43:19 +0100

    Testing SSH access.

--- tic.R
+++ tic.R
@@ -2,6 +2,7 @@ do_package_checks()
 
 if (ci_on_travis()) {
   get_stage("deploy") %>%
+    add_step(step_test_ssh()) %>%
     add_step(step_build_pkgdown()) %>%
     add_step(step_push_deploy(branch = "gh-pages", path = "docs"))
 }

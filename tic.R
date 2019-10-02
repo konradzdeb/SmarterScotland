693d015f01b81ddfa3b877bd6131f056e3e56d6c
Konrad Zdeb <konrad.zdeb@me.com>
2019-10-02 07:37:44 +0100

    Buid
    Created favicon and added step in tic to automatically re-create.

--- tic.R
+++ tic.R
@@ -6,6 +6,8 @@ if (ci_on_travis()) {
 
   get_stage("deploy") %>%
     add_step(step_test_ssh()) %>%
+    add_step(step_run_code(pkgdown::build_favicon(
+      pkg = ".", overwrite = TRUE))) %>%
     add_step(step_build_pkgdown()) %>%
     add_step(step_push_deploy(branch = "gh-pages", path = "docs"))
 }

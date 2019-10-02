5ab1c2f602ab2ea8f84ed97500bc8d9985086fdf
Konrad Zdeb <konrad.zdeb@me.com>
2019-10-02 08:23:44 +0100

    Build Forcing library load.

--- tic.R
+++ tic.R
@@ -3,8 +3,10 @@ do_package_checks()
 if (ci_on_travis()) {
   get_stage("before_deploy") %>%
     add_step(step_install_ssh_keys()) %>%
-    add_code_step(call = build_favicons(pkg = ".", overwrite = TRUE),
-                  prepare_call = library(pkgdown))
+    add_code_step(call = {
+      library(pkgdown)
+      build_favicons(pkg = ".", overwrite = TRUE)
+    })
 
   get_stage("deploy") %>%
     add_step(step_test_ssh()) %>%
